53eb4bc213f07921dc8b4cde42a6722223d0b110
AlehSoda <aleh@sodalabs.xyz>
2026-03-23 13:33:41 +0100

    use Strings from OZ instead of custom funcion

--- wallet/contracts/with-restrictions/RestrictionListIntegration.sol
+++ wallet/contracts/with-restrictions/RestrictionListIntegration.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "./IRestrictionList.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /// @title RestrictionListIntegration
 /// @notice A helper contract that provides restriction list integration functionality
@@ -300,15 +301,6 @@ abstract contract RestrictionListIntegration {
     /// @param addr The address to convert
     /// @return The string representation of the address
     function _addressToString(address addr) internal pure returns (string memory) {
-        bytes32 value = bytes32(uint256(uint160(addr)));
-        bytes memory alphabet = "0123456789abcdef";
-        bytes memory str = new bytes(42);
-        str[0] = '0';
-        str[1] = 'x';
-        for (uint256 i = 0; i < 20; i++) {
-            str[2 + i * 2] = alphabet[uint8(value[i + 12] >> 4)];
-            str[3 + i * 2] = alphabet[uint8(value[i + 12] & 0x0f)];
-        }
-        return string(str);
+        return Strings.toHexString(uint256(uint160(addr)), 20);
     }
 } 
